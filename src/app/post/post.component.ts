4f54cbadb3da2b8371f24f25b1ea40f1ca1cd076
Murat Kondu <murat.kondu.mk@hotmail.com>
2022-11-27 16:45:53 +0300

    User verisi alma ve listeleme

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Post } from './post';
+import { User } from './user';
 @Component({
     selector: 'app-post',
     templateUrl: './post.component.html',
@@ -13,13 +14,16 @@ export class PostComponent {
     }
 
     path: string = "https://jsonplaceholder.typicode.com/posts" //servis url tanımlama
+    userpath: string = "https://jsonplaceholder.typicode.com/users" //servis url tanımlama
 
     posts: Post[];
+    users: User[];
 
     ngOnInit() {
         //uygulama açıldığında postları çekme işlemi için ->HttpClientModule
 
         this.getPosts();
+        this.getUsers();
     }
 
     getPosts() {
@@ -31,6 +35,14 @@ export class PostComponent {
             //bu işlem get yapınca gerçekleşmez buna .subscribe olmak gerekir
             //subscribe bir promise yapısıdır, işlem sonucunda ne olduğu hakkında bilgi verir
             //observable gelen giden detayı izler
+    }
+
+    getUsers() {
+        this.http.get < User[] > (this.userpath).subscribe(response => {
+            //uygulama açıldığında postlar doldurulur
+            this.users = response
+        })
+
     }
 
 }
