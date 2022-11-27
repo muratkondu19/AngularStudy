f98abfec84bfaa53e870ffbe99efb71d84d8f379
Murat Kondu <murat.kondu.mk@hotmail.com>
2022-11-27 13:31:18 +0300

    Component view/html entegrasyonu örneği

--- src/app/customer/customer.component.ts
+++ src/app/customer/customer.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-customer',
-  templateUrl: './customer.component.html',
-  styleUrls: ['./customer.component.scss']
+    selector: 'app-customer',
+    templateUrl: './customer.component.html',
+    styleUrls: ['./customer.component.scss']
 })
 export class CustomerComponent {
-
+    customer: string = "Murat Kondu"
 }
