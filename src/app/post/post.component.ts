32ec7591aca05a4f1690f8ec12f2fbca6d3fdaac
Murat Kondu <murat.kondu.mk@hotmail.com>
2022-11-27 16:31:29 +0300

    htpclient ile servis üzerinden data listeleme

--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Post } from './post';
 @Component({
-  selector: 'app-post',
-  templateUrl: './post.component.html',
-  styleUrls: ['./post.component.scss']
+    selector: 'app-post',
+    templateUrl: './post.component.html',
+    styleUrls: ['./post.component.scss']
 })
 export class PostComponent {
+    //HttpClienti uygulamaya tanıtma
+    constructor(private http: HttpClient) {
+
+    }
+
+    path: string = "https://jsonplaceholder.typicode.com/posts" //servis url tanımlama
+
+    posts: Post[];
+
+    ngOnInit() {
+        //uygulama açıldığında postları çekme işlemi için ->HttpClientModule
+        this.http.get < Post[] > (this.path).subscribe(response => {
+                //uygulama açıldığında postlar doldurulur
+                this.posts = response
+            }) //gelen data response olarak alınır
+            //angular reactive js ile çalışmaktadır, observable çalışmaktadır
+            //bu işlem get yapınca gerçekleşmez buna .subscribe olmak gerekir
+            //subscribe bir promise yapısıdır, işlem sonucunda ne olduğu hakkında bilgi verir
+            //observable gelen giden detayı izler
+    }
 
 }
